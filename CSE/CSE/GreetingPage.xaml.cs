f7269efb53277042fdab74f27364c990ad386cf5
Maxim <Ma.Prokofjev@gmail.com>
2017-09-16 18:17:29 +0300

    Create a new windows forms branch, add login form

---
+++ CSE/CSE/GreetingPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CSE
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class GreetingPage : ContentPage
+	{
+		public GreetingPage ()
+		{
+			InitializeComponent ();
+		}
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("alertas", "hello world", "ok");
+        }
+    }
+}

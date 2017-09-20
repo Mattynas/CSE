42c694a58ff5f0fa32d506b46e7ecdf10b5358b3
JaCobbMedia <joksavodnikas@gmail.com>
2017-09-20 19:36:22 +0300

    Changed Initial class to LoginForm and added comment about ImageAnalysisMenu

--- WindowsFormsCSE/WindowsFormsCSE/Program.cs
+++ WindowsFormsCSE/WindowsFormsCSE/Program.cs
@@ -11,7 +11,8 @@ namespace WindowsFormsCSE
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new ImageAnalysisMenu());
+            Application.Run(new LoginForm());
+            //Application.Run(new ImageAnalysisMenu()); If you want to test basic tesseract image recognition
         }
     }
 }

91e9af06836957b630c89f915c6969edbbbbd632
Mattynas <marynas21@gmail.com>
2017-10-15 14:27:41 +0300

    It should have less conflicts

---
+++ WindowsFormsCSE/WindowsFormsCSE/GUI/RegisterForm.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace WindowsFormsCSE.GUI
+{
+    partial class RegisterForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UsernameLabel = new System.Windows.Forms.Label();
+            this.EmailLabel = new System.Windows.Forms.Label();
+            this.PasswordLabel = new System.Windows.Forms.Label();
+            this.ConfirmPassLabel = new System.Windows.Forms.Label();
+            this.UsernameTextBox = new System.Windows.Forms.TextBox();
+            this.EmailTextBox = new System.Windows.Forms.TextBox();
+            this.PasswordTextBox = new System.Windows.Forms.TextBox();
+            this.ConfirmPassTextBox = new System.Windows.Forms.TextBox();
+            this.RegisterButton = new System.Windows.Forms.Button();
+            this.CancelButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // UsernameLabel
+            // 
+            this.UsernameLabel.AutoSize = true;
+            this.UsernameLabel.Location = new System.Drawing.Point(152, 50);
+            this.UsernameLabel.Name = "UsernameLabel";
+            this.UsernameLabel.Size = new System.Drawing.Size(55, 13);
+            this.UsernameLabel.TabIndex = 0;
+            this.UsernameLabel.Text = "Username";
+            // 
+            // EmailLabel
+            // 
+            this.EmailLabel.AutoSize = true;
+            this.EmailLabel.Location = new System.Drawing.Point(163, 108);
+            this.EmailLabel.Name = "EmailLabel";
+            this.EmailLabel.Size = new System.Drawing.Size(32, 13);
+            this.EmailLabel.TabIndex = 1;
+            this.EmailLabel.Text = "Email";
+            // 
+            // PasswordLabel
+            // 
+            this.PasswordLabel.AutoSize = true;
+            this.PasswordLabel.Location = new System.Drawing.Point(152, 161);
+            this.PasswordLabel.Name = "PasswordLabel";
+            this.PasswordLabel.Size = new System.Drawing.Size(53, 13);
+            this.PasswordLabel.TabIndex = 2;
+            this.PasswordLabel.Text = "Password";
+            // 
+            // ConfirmPassLabel
+            // 
+            this.ConfirmPassLabel.AutoSize = true;
+            this.ConfirmPassLabel.Location = new System.Drawing.Point(133, 216);
+            this.ConfirmPassLabel.Name = "ConfirmPassLabel";
+            this.ConfirmPassLabel.Size = new System.Drawing.Size(91, 13);
+            this.ConfirmPassLabel.TabIndex = 3;
+            this.ConfirmPassLabel.Text = "Confirm Password";
+            // 
+            // UsernameTextBox
+            // 
+            this.UsernameTextBox.Location = new System.Drawing.Point(115, 66);
+            this.UsernameTextBox.Name = "UsernameTextBox";
+            this.UsernameTextBox.Size = new System.Drawing.Size(125, 20);
+            this.UsernameTextBox.TabIndex = 4;
+            // 
+            // EmailTextBox
+            // 
+            this.EmailTextBox.Location = new System.Drawing.Point(115, 124);
+            this.EmailTextBox.Name = "EmailTextBox";
+            this.EmailTextBox.Size = new System.Drawing.Size(125, 20);
+            this.EmailTextBox.TabIndex = 5;
+            // 
+            // PasswordTextBox
+            // 
+            this.PasswordTextBox.Location = new System.Drawing.Point(115, 177);
+            this.PasswordTextBox.Name = "PasswordTextBox";
+            this.PasswordTextBox.Size = new System.Drawing.Size(125, 20);
+            this.PasswordTextBox.TabIndex = 6;
+            // 
+            // ConfirmPassTextBox
+            // 
+            this.ConfirmPassTextBox.Location = new System.Drawing.Point(115, 232);
+            this.ConfirmPassTextBox.Name = "ConfirmPassTextBox";
+            this.ConfirmPassTextBox.Size = new System.Drawing.Size(125, 20);
+            this.ConfirmPassTextBox.TabIndex = 7;
+            // 
+            // RegisterButton
+            // 
+            this.RegisterButton.Location = new System.Drawing.Point(210, 296);
+            this.RegisterButton.Name = "RegisterButton";
+            this.RegisterButton.Size = new System.Drawing.Size(75, 23);
+            this.RegisterButton.TabIndex = 8;
+            this.RegisterButton.Text = "Register";
+            this.RegisterButton.UseVisualStyleBackColor = true;
+            this.RegisterButton.Click += new System.EventHandler(this.RegisterButton_Click);
+            // 
+            // CancelButton
+            // 
+            this.CancelButton.Location = new System.Drawing.Point(52, 296);
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.Size = new System.Drawing.Size(75, 23);
+            this.CancelButton.TabIndex = 9;
+            this.CancelButton.Text = "Cancel";
+            this.CancelButton.UseVisualStyleBackColor = true;
+            this.CancelButton.Click += new System.EventHandler(this.CancelButton_Click);
+            // 
+            // RegisterForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(348, 345);
+            this.Controls.Add(this.CancelButton);
+            this.Controls.Add(this.RegisterButton);
+            this.Controls.Add(this.ConfirmPassTextBox);
+            this.Controls.Add(this.PasswordTextBox);
+            this.Controls.Add(this.EmailTextBox);
+            this.Controls.Add(this.UsernameTextBox);
+            this.Controls.Add(this.ConfirmPassLabel);
+            this.Controls.Add(this.PasswordLabel);
+            this.Controls.Add(this.EmailLabel);
+            this.Controls.Add(this.UsernameLabel);
+            this.Name = "RegisterForm";
+            this.Text = "Register";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.RegisterForm_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label UsernameLabel;
+        private System.Windows.Forms.Label EmailLabel;
+        private System.Windows.Forms.Label PasswordLabel;
+        private System.Windows.Forms.Label ConfirmPassLabel;
+        private System.Windows.Forms.TextBox UsernameTextBox;
+        private System.Windows.Forms.TextBox EmailTextBox;
+        private System.Windows.Forms.TextBox PasswordTextBox;
+        private System.Windows.Forms.TextBox ConfirmPassTextBox;
+        private System.Windows.Forms.Button RegisterButton;
+        private System.Windows.Forms.Button CancelButton;
+    }
+}
---
+++ WindowsFormsCSE/WindowsFormsCSE/GUI/RegisterForm.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsCSE.XML;
+
+namespace WindowsFormsCSE.GUI
+{
+    public partial class RegisterForm : Form
+    {
+        public RegisterForm()
+        {
+            InitializeComponent();
+        }
+
+        private void RegisterButton_Click(object sender, EventArgs e)
+        {
+            if (PasswordTextBox.Text != ConfirmPassTextBox.Text)
+            {
+                
+            }
+            else if (!EmailTextBox.Text.Contains("@"))
+            {
+                //Make Regex
+                
+
+            }
+            else if (UsersXML.Register(UsernameTextBox.Text, PasswordTextBox.Text, EmailTextBox.Text))
+            {
+
+                string message = "Registration is successful!";
+                string caption = "Success";
+                var button = MessageBoxButtons.OK;
+
+                var messagebox = MessageBox.Show(message, caption, button);
+                this.Close();
+            }
+            else
+            {
+                string message = "Registration failed";
+                string caption = "Failed";
+                var button = MessageBoxButtons.OK;
+                var messagebox = MessageBox.Show(message, caption, button);
+            }
+        }
+
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void RegisterForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            var login = new LoginForm();
+            login.Show();
+        }
+    }
+}

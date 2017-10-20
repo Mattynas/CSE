2007fdd6283b1cf8396d12b711ee1f2bf9d20ae8
Mattynas <marynas21@gmail.com>
2017-10-20 22:52:33 +0300

    Made RegisterForm use RegisterDataValidation

--- WindowsFormsCSE/WindowsFormsCSE/GUI/RegisterForm.Designer.cs
+++ WindowsFormsCSE/WindowsFormsCSE/GUI/RegisterForm.Designer.cs
@@ -116,7 +116,6 @@
             this.RegisterButton.Text = "Register";
             this.RegisterButton.UseVisualStyleBackColor = true;
             this.RegisterButton.Click += new System.EventHandler(this.RegisterButton_Click);
-            this.RegisterButton.KeyDown += new System.Windows.Forms.KeyEventHandler(this.RegisterButton_KeyDown);
             // 
             // CancelButton
             // 

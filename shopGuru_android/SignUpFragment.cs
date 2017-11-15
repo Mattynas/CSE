1f3cd8a2c08f5bd26a9ce2e1ad0f092ae6ab841d
Maxim <Ma.Prokofjev@gmail.com>
2017-11-15 20:37:31 +0200

    merged

---
+++ shopGuru_android/SignUpFragment.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace shopGuru_android
+{
+    public class SignUpFragment : DialogFragment
+    {
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your fragment here
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+           
+            base.OnCreateView(inflater, container, savedInstanceState);
+
+            var view = inflater.Inflate(Resource.Layout.dialog_signUp, container, false);
+
+            return view;
+        }
+    }
+}
